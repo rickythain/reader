f4741b7dd9177859396e8cac256fccbadfb03c10
ricky-deriv <ricky-deriv@deriv.com>
2023-05-26 21:13:05 +0800

    create tables and constraints

--- db/init-scripts/01_create_tables.sql
+++ db/init-scripts/01_create_tables.sql
@@ -1,6 +1,19 @@
 CREATE TABLE IF NOT EXISTS book (
-  isbn INT PRIMARY KEY,
+  isbn VARCHAR PRIMARY KEY,
   title VARCHAR
 );
 
+CREATE TABLE IF NOT EXISTS author (
+  author_id VARCHAR PRIMARY KEY,
+  author_name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS book_author (
+  book_author_id VARCHAR PRIMARY KEY
+  isbn VARCHAR
+  author_id VARCHAR
+
+  FOREIGN KEY (isbn) REFERENCES book(isbn)
+  FOREIGN KEY (author_id) REFERENCES author(author_id)
+);
 
--- db/init-scripts/02_create_constraints.sql
+++ db/init-scripts/02_create_constraints.sql
@@ -1,3 +1,12 @@
 ALTER TABLE book
-    ADD CONSTRAINT isbn_length CHECK (LENGTH(isbn::TEXT) = 10 OR LENGTH(isbn::TEXT) = 13),
-    ADD CONSTRAINT title_length CHECK (LENGTH(title) <= 100);
+    ADD CONSTRAINT chk_isbn_length CHECK (LENGTH(isbn) IN (10, 13)),
+    ADD CONSTRAINT chk_title_length CHECK (LENGTH(title) <= 100);
+
+ALTER TABLE author
+    ADD CONSTRAINT chk_authorId_length CHECK (LENGTH(author_id) = 10),
+    ADD CONSTRAINT chk_authorName_length CHECK (LENGTH(author_name) <= 100);
+
+ALTER TABLE book_author
+    ADD CONSTRAINT chk_bookAuthorId_length CHECK (LENGTH(book_author_id) = 10),
+    ADD CONSTRAINT chk_isbn_length CHECK (LENGTH(isbn) IN (10, 13)),
+    ADD CONSTRAINT chk_authorId_length CHECK (LENGTH(author_id) = 10),
