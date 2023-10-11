dd6a086102c3d7196354e50d381597f2a098680b
Ricky Espineli Thain <ricky.thain@deriv.com>
2023-10-11 10:50:08 +0800

    update api service to receive rest request and communicate using grpc with internal service

--- server/api/api.js
+++ server/api/api.js
@@ -1,7 +1,42 @@
-const server = require("express")();
+const express = require('express');
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
-server.listen(3001, async () => { });
+const app = express();
+app.use(express.json());
 
-server.get("/test", async (_, response) => {
-    response.json({ "response": "connection established hi from developer!" });
+const restPort = 5000;
+
+// Load gRPC proto definition and create a gRPC client
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, './protos/products.proto'));
+const productsProto = grpc.loadPackageDefinition(packageDefinition);
+const productsStub = new productsProto.Products('product:50051',
+                        grpc.credentials.createInsecure());
+
+function getBooks() {
+    return new Promise((resolve, reject) => {
+        productsStub.getBooks({}, (err, books) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log('got a list of books...');
+                resolve(books);
+            }
+        });
+    });
+}
+
+app.get('/books', async (req, res) => {
+    try {
+        const books = await getBooks();
+        res.json(books);
+    } catch (error) {
+        console.error('Error while fetching books: ', error);
+        res.status(500).json({ error: 'Failed to fetch books'});
+    }
+});
+
+app.listen(restPort, () => {
+    console.log(`API is listening on port ${restPort}`);
 });
